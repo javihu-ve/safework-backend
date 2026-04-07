25f25025d0476c8be3fd0ae8605dd81cdfd40f18
Lalobatimoledor <jeduardovazbar@gmail.com>
2026-04-06 20:18:13 -0600

    escalar
    se agrega la funcion de escalar

--- src/controllers/ReportController.js
+++ src/controllers/ReportController.js
@@ -35,3 +35,47 @@ exports.getReportes = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error", message: error.message });
     }
 };
+
+// Endpoint 3.1: Escalar / Actualizar Estado
+exports.escalarReporte = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nuevoEstado, comentariosSupervisor } = req.body;
+
+        // 1. Verificar jerarquía: Solo Supervisor o Gerente pueden escalar
+        if (req.user.rol === 'Operador') {
+            return res.status(403).json({ 
+                error: "Forbidden", 
+                message: "No tienes la jerarquía necesaria para escalar este incidente." 
+            });
+        }
+
+        // 2. Buscar y actualizar el reporte
+        const reporteActualizado = await Reporte.findByIdAndUpdate(
+            id,
+            { 
+                estado: nuevoEstado,
+                $push: { historialComentarios: { 
+                    autor: req.user.id, 
+                    comentario: comentariosSupervisor,
+                    fecha: new Date() 
+                }}
+            },
+            { new: true } // Para que devuelva el objeto ya cambiado
+        );
+
+        if (!reporteActualizado) {
+            return res.status(404).json({ error: "Not Found", message: "Reporte no encontrado." });
+        }
+
+        res.status(200).json({
+            _id: reporteActualizado._id,
+            estado: reporteActualizado.estado,
+            ultimaActualizacion: new Date(),
+            mensaje: "El incidente ha sido actualizado correctamente."
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error", message: error.message });
+    }
+};
--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createReporte, getReportes } = require("../controllers/ReportController");
+const { createReporte, getReportes, escalarReporte } = require("../controllers/ReportController");
 const { verificarToken } = require("../middlewares/authMiddleware");
 
 // Endpoints según el contrato [source: 10]
 router.post("/createReporte",verificarToken, createReporte); // Fíjate que es sin la 's' al final
 router.get("/getAllReports",verificarToken, getReportes);
+router.put("/:id/escalar", verificarToken, escalarReporte);
 
 module.exports = router;
